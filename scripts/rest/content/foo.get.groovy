2ca9ff3a38fd324ae94600326162183434f1dee0
admin admin <evaladmin@example.com>
2017-12-19 16:34:33 -0500

    Wrote content /scripts/rest/content/foo.get.groovy

--- scripts/rest/content/foo.get.groovy
+++ scripts/rest/content/foo.get.groovy
@@ -3,9 +3,11 @@ import acme.Fox
 def cart = [:]
 cart.name = (params.name) ? params.name : "default"
 cart.itemCount = 4
+
 cart.goods =  [ "Apples", "Oranges", "Grapes", "Peaches" ]
 
 cart.message = new Fox().speak()
-cart.sum = 40 + 40 + 40
+
+cart.sum = 50 + 50 + 50
 
 return cart
