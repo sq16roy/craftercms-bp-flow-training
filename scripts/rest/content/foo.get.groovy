c0ec848ff5d560abfc8ce55e120f36bfa8ff86b4
admin admin <evaladmin@example.com>
2018-04-13 10:03:07 -0400

    Wrote content /scripts/rest/content/foo.get.groovy

--- scripts/rest/content/foo.get.groovy
+++ scripts/rest/content/foo.get.groovy
@@ -6,6 +6,8 @@ cart.itemCount = 100 + 50
 
 cart.goods =  [ "Grapes 2", "Oranges", "Grapes", "Peaches", "Plums", "Apples" ]
 
+cart.myContent = siteItemService.getSiteItem("/site/website/en/index.xml")
+
 
 
 return cart
