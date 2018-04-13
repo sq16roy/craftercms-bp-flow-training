7a897eb8eda7529f7495fc2fbcb847f9557b5015
admin admin <evaladmin@example.com>
2018-04-13 10:02:06 -0400

    Wrote content /scripts/rest/content/foo.get.groovy

--- scripts/rest/content/foo.get.groovy
+++ scripts/rest/content/foo.get.groovy
@@ -4,7 +4,7 @@ def cart = [:]
 cart.name = (params.name) ? params.name : "default"
 cart.itemCount = 100 + 50
 
-cart.goods =  [ "Grapes 2", "Oranges", "Grapes", "Peaches", "Plums" ]
+cart.goods =  [ "Grapes 2", "Oranges", "Grapes", "Peaches", "Plums", "Apples" ]
 
 
 
