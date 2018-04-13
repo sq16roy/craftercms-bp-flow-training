b5a86d0117d76450732e6b4b77d201b5418f9727
admin admin <evaladmin@example.com>
2018-04-13 09:33:32 -0400

    Wrote content /scripts/pages/entry.groovy

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -5,6 +5,3 @@ def targetedContentService = applicationContext.get("targetedContentService")
 def homepageScenarioItem = targetedContentService.getHomepageScenario(profile)
 
 templateModel.homepageScenario = homepageScenarioItem
-
-// make a rest call
-templateModel.mySum = 40 + 50
