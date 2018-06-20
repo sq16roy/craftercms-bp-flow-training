a0a82324abe2d92fabb3c1f3799b0dc94e87287a
John Demo <john@demo.com>
2018-06-20 12:33:45 -0400

    Wrote content /scripts/rest/content/foo.get.groovy

--- scripts/rest/content/foo.get.groovy
+++ scripts/rest/content/foo.get.groovy
@@ -4,11 +4,12 @@ def cart = [:]
 cart.name = (params.name) ? params.name : "default"
 cart.itemCount = 100 + 50
 
-cart.goods =  [ "Grapes 2", "Oranges", "Grapes", "Peaches", "Plums", "Apples" ]
+cart.goods =  [ "Starfruit", "Oranges", "Grapes", "Peaches", "Plums", "Apples" ]
 
 cart.myContent = [:]
 cart.myContent.headline = siteItemService.getSiteItem("/site/website/en/index.xml").queryValue("headline")
 cart.myContent.subheadline = siteItemService.getSiteItem("/site/website/en/index.xml").queryValue("subHeadline")
+cart.myContent.subheadline = siteItemService.getSiteItem("/site/website/en/our-demo/index.xml").queryValue("dateline")
 
 
 
