df616f8547c680edfa990c600b5316d2288572a3
John Demo <john@demo.com>
2018-06-20 12:34:35 -0400

    Wrote content /scripts/rest/content/foo.get.groovy

--- scripts/rest/content/foo.get.groovy
+++ scripts/rest/content/foo.get.groovy
@@ -9,7 +9,7 @@ cart.goods =  [ "Starfruit", "Oranges", "Grapes", "Peaches", "Plums", "Apples" ]
 cart.myContent = [:]
 cart.myContent.headline = siteItemService.getSiteItem("/site/website/en/index.xml").queryValue("headline")
 cart.myContent.subheadline = siteItemService.getSiteItem("/site/website/en/index.xml").queryValue("subHeadline")
-cart.myContent.subheadline = siteItemService.getSiteItem("/site/website/en/our-demo/index.xml").queryValue("dateline")
+cart.myContent.dateline = siteItemService.getSiteItem("/site/website/en/services/index.xml").queryValue("dateline")
 
 
 
