81f4fe547ff91a6f8c1a3402acd66ef5bea6f9b7
Paras Jain <pjain@rivetlogic.com>
2018-08-13 16:47:08 -0400

    modified entry.groovy to get alerts

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -8,3 +8,17 @@ templateModel.homepageScenario = homepageScenarioItem
 
 def mySum = 10 + 100 + 50
 templateModel.mySum = mySum + 5
+
+def queryStatement = "content-type:\"/component/alerts\""
+def searchService = targetedContentService.getSearchService()
+def query = searchService.createQuery()
+query.setQuery(queryStatement)
+
+def executedQuery = searchService.search(query)
+
+def matches = [:]
+matches.found = executedQuery.response.numFound
+matches.events = executedQuery.response.documents
+matches.query = queryStatement
+
+results = matches.events
