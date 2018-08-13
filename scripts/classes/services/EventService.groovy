d129b08b7807fb83a879cd62f6177f2cca7dfed1
Paras Jain <pjain@rivetlogic.com>
2018-08-13 13:27:40 -0400

    Initial upload

---
+++ scripts/classes/services/EventService.groovy
@@ -0,0 +1,37 @@
+package services
+
+/**
+ * Event service
+ */
+public class EventService {
+
+    private searchService
+    public getSearchService() { return searchService }
+    public setSearchService(service) { searchService = service }
+
+    /**
+     * Get Events for date range
+     * @return returns Hub Articles
+     */
+    public getEvents(startDate, endDate) {
+
+        def results = null
+        def queryStatement = "content-type:\"/component/event\""
+
+        def query = searchService.createQuery()
+        query.setQuery(queryStatement)
+
+        def executedQuery = searchService.search(query)
+
+        def matches = [:]
+        matches.found = executedQuery.response.numFound
+        matches.events = executedQuery.response.documents
+        matches.query = queryStatement
+
+        results = matches.events
+
+        return results
+    }
+
+
+}
