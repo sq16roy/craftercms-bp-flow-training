c852cf8611332b79afe8d1b2b590e4f6b01f19c3
Paras Jain <pjain@rivetlogic.com>
2018-08-14 14:59:31 -0400

    Fixing events json

--- scripts/classes/services/EventService.groovy
+++ scripts/classes/services/EventService.groovy
@@ -17,7 +17,7 @@ public class EventService {
     public getEvents(startDateStr, endDateStr) {
 
         def results = null
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+        //SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
         //def startDate = formatDateAsIso(formatter.parse(startDateStr))
 
         def queryStatement = "content-type:\"/page/events\""
@@ -29,14 +29,21 @@ public class EventService {
 
         def executedQuery = searchService.search(query)
 
-        def matches = [:]
-        matches.found = executedQuery.response.numFound
-        matches.events = executedQuery.response.documents
-        matches.query = queryStatement
+        results = executedQuery.response.documents
+        def modifiedResults = []
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
 
-        results = matches.events
+        for (result in results){
+            if(result.startDate != null){
+                result.start = dateFormat.parse(result.startDate).getTime();
+            }
+            if(result.endDate != null) {
+                result.end = dateFormat.parse(result.endDate).getTime();
+            }
+            modifiedResults.add(result)
+        }
 
-        return results
+        return modifiedResults
     }
 
 }
