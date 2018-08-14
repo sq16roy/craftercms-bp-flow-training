17f402804654da13a6c8a5df533f6d573f6598ef
Paras Jain <pjain@rivetlogic.com>
2018-08-14 17:52:48 -0400

    getting actual events

--- static-assets/app/calendar/scripts/calendar.js
+++ static-assets/app/calendar/scripts/calendar.js
@@ -10,8 +10,8 @@
         var DEPARTMENTS_URL     = 'http://127.0.0.1:8080/api/1/services/departments/departments.json';
         var SEARCH_URL          = 'http://127.0.0.1:8080/api/1/services/search.json';
     } else {
-        //var EVENTS_URL          = '/api/1/services/event/events.json';
-        var EVENTS_URL          = '/static-assets/api/data.json';
+        var EVENTS_URL          = '/api/1/services/event/events.json';
+        //var EVENTS_URL          = '/static-assets/api/data.json';
         var CREATE_EVENT_URL    = '/static-assets/api/data.json';
         var UPDATE_EVENT_URL    = '/static-assets/api/data.json';
         var DELETE_EVENT_URL    = '/static-assets/api/data.json';
