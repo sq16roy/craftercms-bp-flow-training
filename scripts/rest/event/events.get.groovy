ed1e35049e907159483cc577089981fe18d302c4
Paras Jain <pjain@rivetlogic.com>
2018-08-14 14:27:07 -0400

    Removing null checks for events

--- scripts/rest/event/events.get.groovy
+++ scripts/rest/event/events.get.groovy
@@ -4,15 +4,15 @@ def endDateParam = params.end
 
 def eventService = applicationContext.get("eventService")
 
-if(startDateParam == null || endDateParam == null) {
-    response.setStatus(404)
-}
-else {
+//if(startDateParam == null || endDateParam == null) {
+//    response.setStatus(404)
+//}
+//else {
     retEvents = eventService.getEvents(startDateParam, endDateParam)
 
     if(retEvents == null) {
         response.setStatus(404)
     }
-}
+//}
 
 return retEvents
