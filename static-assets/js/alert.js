45b2561f7623e78bd8891e03828a147fef545a6a
Roy Solera <roysolera@Roy-Solera.local>
2018-08-17 14:33:03 -0600

    show and hide feature

--- static-assets/js/alert.js
+++ static-assets/js/alert.js
@@ -1,31 +1,23 @@
 (function () {
     let alerts = document.getElementById('alertBoxContainer');
-
-    if (alerts.childElementCount <= 1) {
-        console.log('here')
-        document.getElementById('alertBoxContainer').style.display = 'none';
-    } else {
-        let state = false;
-        let hideBox = setTimeout(function() { 
+    let state = false;
+    let hideBox = setTimeout(function() { 
+        if (!state) {
+            $('#alertBoxContainer').fadeOut();
+        }
+    }, 3000);
+    alerts.addEventListener("mouseover", () => {
+        state = true;
+        clearTimeout(hideBox);
+    });
+    alerts.addEventListener("mouseleave", () => {
+        state = false;
+        hideBox = setTimeout(function() { 
             if (!state) {
                 $('#alertBoxContainer').fadeOut();
             }
         }, 3000);
-
-        document.getElementById('alertBoxContainer').style.display = 'block';
-        alerts.addEventListener("mouseover", () => {
-            state = true;
-            clearTimeout(hideBox);
-        });
-        alerts.addEventListener("mouseleave", () => {
-            state = false;
-            hideBox = setTimeout(function() { 
-                if (!state) {
-                    $('#alertBoxContainer').fadeOut();
-                }
-            }, 3000);
-        });
-    }
+    });
     document.getElementById('closeBoxAlert').addEventListener('click', function() {
         $('#alertBoxContainer').fadeOut();
     });
